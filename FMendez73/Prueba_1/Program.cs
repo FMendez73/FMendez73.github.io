fcfabb19f62e3936f7c4a9b27222e38295f258e7
fmendez73 <fmendez73@uabc.edu.mx>
2022-09-03 01:30:30 -0700

    Practica_2-Sep02

---
+++ FMendez73/Prueba_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Prueba_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas;
+            float costoHora;
+            float sueldo;
+            string linea;
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
